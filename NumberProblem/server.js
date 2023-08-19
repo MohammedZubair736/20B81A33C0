bdec20264eb01976fcecb75981626bb3c81e8b6e
MohammedZubair736 <78092448+MohammedZubair736@users.noreply.github.com>
2023-08-19 15:44:58 +0530

    Update server.js

--- NumberProblem/server.js
+++ NumberProblem/server.js
@@ -1,1 +1,55 @@
 
+
+async function primes(set) {
+    var response = await fetch("http://20.244.56.144/numbers/primes");
+    var data=await response.json()
+    //console.log(data.numbers);
+    var arr=data.numbers;
+    await arr.forEach((item)=>{
+        set.add(item);
+    })
+    
+}
+ async function fibo(set){
+    var response = await fetch("http://20.244.56.144/numbers/fibo");
+    var data=await response.json()
+    var arr=data.numbers;
+    await arr.forEach((item)=>{
+        set.add(item);
+    })
+    var arr=data;
+   // console.log(set);
+}
+
+async function odd(set){
+
+    var response = await fetch("http://20.244.56.144/numbers/odd");
+    var data=await response.json()
+    var arr=data.numbers;
+    await arr.forEach((item)=>{
+        set.add(item);
+    })
+   // console.log(set);
+}
+async function even(set){
+    var response = await fetch("http://20.244.56.144/numbers/rand");
+    var data=await response.json()
+    var arr=data.numbers;
+    await arr.forEach((item)=>{
+        set.add(item);
+    })
+    //console.log(set);
+  } 
+  async function func(){
+  let set=new Set();
+  await primes(set);
+  await fibo(set);
+  await odd(set);
+  await even(set);
+
+   
+  console.log(set);
+  
+  }
+  func();
+
